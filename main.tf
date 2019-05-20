9da3ea3dff3670e26b803396872694e2133e71e2
Pam Selle <pamela.selle@gmail.com>
2019-05-20 11:56:53 -0400

    update ref

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.5.4"
+  source     = "git::https://github.com/pselle/terraform-null-label.git"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.name
