3716ceb5be01bcb04919e65b293e34157cee08ee
Pam Selle <pamela.selle@gmail.com>
2019-05-20 11:34:07 -0400

    0.12 upgrade

--- variables.tf
+++ variables.tf
@@ -11,18 +11,19 @@ variable "stage" {
 }
 
 variable "attributes" {
-  type    = "list"
+  type    = list(string)
   default = []
 }
 
 variable "tags" {
-  type        = "map"
-  default     = {}
+  type = map(string)
+  default = {
+  }
   description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`)"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
 }
@@ -41,3 +42,4 @@ variable "enabled" {
   description = "Set to false to prevent the module from creating any resources"
   default     = "true"
 }
+
